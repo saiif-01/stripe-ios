f9432877ccf5d288fd1c27108486071f18a96cdb
Nick Porter <porter@stripe.com>
2026-09-16 13:20:29 -0400

    Use legacy customer keys in TrueMoney UI tests

--- Example/PaymentSheet Example/PaymentSheetUITest/PaymentSheetTrueMoneyUITests.swift
+++ Example/PaymentSheet Example/PaymentSheetUITest/PaymentSheetTrueMoneyUITests.swift
@@ -25,6 +25,7 @@ class PaymentSheetTrueMoneyUITests: PaymentSheetStandardLPMUICase {
         var settings = PaymentSheetTestPlaygroundSettings.defaultValues()
         settings.layout = .horizontal
         settings.customerMode = .new
+        settings.customerKeyType = .legacy
         settings.apmsEnabled = .off
         settings.applePayEnabled = .off
         settings.currency = .thb
