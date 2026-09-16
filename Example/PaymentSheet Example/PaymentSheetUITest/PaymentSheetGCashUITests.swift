181020a5dc085386dbca751949327354c0bb5784
Nick Porter <porter@stripe.com>
2026-09-16 13:20:26 -0400

    Use legacy customer keys in GCash UI tests

--- Example/PaymentSheet Example/PaymentSheetUITest/PaymentSheetGCashUITests.swift
+++ Example/PaymentSheet Example/PaymentSheetUITest/PaymentSheetGCashUITests.swift
@@ -25,6 +25,7 @@ class PaymentSheetGCashUITests: PaymentSheetStandardLPMUICase {
         var settings = PaymentSheetTestPlaygroundSettings.defaultValues()
         settings.layout = .horizontal
         settings.customerMode = .new
+        settings.customerKeyType = .legacy
         settings.apmsEnabled = .off
         settings.applePayEnabled = .off
         settings.currency = .php
